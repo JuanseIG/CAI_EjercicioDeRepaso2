718b56cee43448d33a6c0df09c60d84d1d2f7a89
JuanseIG <89666312+JuanseIG@users.noreply.github.com>
2021-09-06 22:30:53 -0300

    Ejercicio de repaso 2 resuelto
    Ejercicio 2 resuelto

---
+++ CAI_EjercicioDeRepaso2/CAI_EjercicioDeRepaso2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAI_EjercicioDeRepaso2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 0;
+            int nro_registro = 976;
+            string contador = "";
+            int i;
+            
+
+            do
+            {
+                bool primo = false;
+                numero = numero + 1;
+                for (i = 2;i < numero; i++)
+                {
+                    if (numero % i == 0)
+                    {
+                        primo = true;
+                        break;
+                    }
+                }
+
+                if(primo==true)
+                {
+                    if(numero%3==0)
+                    {
+                        contador = contador + numero + " - es divisible por 3 " + "\n";
+                    }
+                    else
+                    {
+                        contador = contador + numero + "\n";
+                    }
+                }
+                    else
+                    {
+                        contador = contador + numero + " - es primo " + "\n";
+                    }
+             
+            } while (numero < nro_registro);
+
+            Console.WriteLine(contador);
+            Console.ReadKey();
+        }
+    }
+}
